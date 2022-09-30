e49c19a81a287b1b3f85d062feb6d4f15eca82b4
Shaniza Lingle <shanizalingle@gmail.com>
2022-09-30 15:32:19 -0700

    add route to vendors and html files

---
+++ Bakery/Controllers/VendorController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+
+namespace Bakery.Controllers
+{
+  public class VendorsController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorLocation)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorLocation);
+      return RedirectToAction("Index");
+    }
+
+    
+  }
+}
